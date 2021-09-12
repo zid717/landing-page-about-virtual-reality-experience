9aa80fc8f59ef6d17710925b00adf079ba39f649
Md. Bayzid Hossain <90511933+zid717@users.noreply.github.com>
2021-09-13 04:52:15 +0600

    virtual reality experience landing page

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+    var a = document.getElementById("hey");
+    var b = document.getElementById("mBar");
+    var c = document.getElementById("navbarSupportedContent");
+
+
+    b.addEventListener('click',function(){
+
+        a.classList.toggle("bg-dark");
+        a.style.transition = "all 1s ease";
+        c.style.margin = "0 0 0 36%";
+    })
+
+   
+
+  
+
+  
+    
+
+
+
+
